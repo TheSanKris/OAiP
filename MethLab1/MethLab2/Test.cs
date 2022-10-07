6f604bec2e89a686c689f99232cd364a6d3edfa7
TheSanKris <Mironovshsa@mail.ru>
2022-10-07 21:18:37 +0300

    Lab2

---
+++ MethLab1/MethLab2/Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethLab1
+{
+    internal class Test
+    {
+       
+        public string Name { get; set; }
+        public bool IsPass { get; set; }
+
+        public Test(string name, bool isPass)
+        {
+            Name = name;
+            IsPass = isPass;
+        }
+
+        public Test()
+        {
+            Name = "undefined";
+            IsPass = false;
+        }
+
+        public override string? ToString()
+        {
+            return Name + " " + IsPass;
+        }
+    }
+}
