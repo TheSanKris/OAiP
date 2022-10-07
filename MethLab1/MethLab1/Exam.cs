185ab6282b1b253eb0f8845b2f4939c65e298098
TheSanKris <Mironovshsa@mail.ru>
2022-10-07 22:32:34 +0300

    Lab2 Remaike

---
+++ MethLab1/MethLab1/Exam.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethLab1
+{
+    class Exam
+    {
+        public string Name { get; set; }
+        public int grade { get; set; }
+        public System.DateTime DateTime { get; set; }
+
+        public Exam(string name, int grade, DateTime dateTime)
+        {
+            Name = name;
+            this.grade = grade;
+            DateTime = dateTime;
+        }
+
+        public Exam()
+        {
+            Name = "Undefind";
+            grade = 2;
+            DateTime = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            string output = $"Название: {Name} Оценка: {grade} Дата экзамена: {DateTime}";
+            return output;
+        }
+    }
+}
---
+++ MethLab1/MethLab1/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace MethLab1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Person person1 = new Person("Alexandr", "Alexandrov", new DateTime(2022, 10, 7));
+            Person person2 = new Person("Alexandr", "Alexandrov", new DateTime(2022, 10, 7));
+
+            Console.WriteLine(person1.Equals(person2));
+            Console.WriteLine(person1 == person2);
+            Console.WriteLine($"Хэш1: {person1.GetHashCode()} Хэш2: {person2.GetHashCode()} ");
+
+            Student student = new Student();
+            student.AddExams(new Exam("OAIP", 5, DateTime.Now));
+            student.AddExams(new Exam());
+            student.AddTest(new Test());
+
+            Console.WriteLine(student.ToString());
+
+            Student studentClone = (Student)student.DeepCopy();
+            student.FirstName = "Alexandr";
+            Console.WriteLine();
+            Console.WriteLine($"Исходный объект: {student}");
+            Console.WriteLine($"Склонированный объект: {studentClone}");
+
+            try
+            {
+                student.GroupNumber = 0;
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine();
+
+            foreach(var l in student.EnumElements())
+            {
+                Console.WriteLine(l.ToString());
+            }
+
+            Console.WriteLine();
+
+            foreach (var exam in student.ExamsGradeOver(3))
+            {
+                Console.WriteLine(exam.ToString());
+            }
+
+        }
+        
+        
+       
+    }
+}
